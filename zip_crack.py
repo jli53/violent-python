80a0f8744d58c8ff8d317a5a0c376d2c047afef6
root <root@localhost.localdomain>
2015-11-08 10:27:06 -0500

    update zipfile cracker

--- zip_crack.py
+++ zip_crack.py
@@ -9,25 +9,9 @@ def test_pass(zFile, password):
     print '[+] Found password' + password + '\n'
   except:
     pass
-'''
-  f_zip = zipfile.ZipFile(filename)
-  passfile = open("/usr/share/wordlists/rockyou.txt","r")
-  for password in passfile.readlines():
-    password = password.strip('\n')
-    #password = "1234"
-    try:
-      print "trying " + password
-      f_zip.extractall(pwd = password)
-      print "[+] Password FOUND!!! It is " + password + "\n"
-      return
-    except Exception, e:
-      #print e
-      pass
-  print "[-] NOT FOUND!" 
-'''
 def main():
   if len(sys.argv) != 2:
-    print "[-] Usage: " + sys.argv[0] + " zipfilename"
+    print "[-] Usage: " + sys.argv[0] + " <zipfilename>\nthis program use kali password dictionary 'rockyou.txt'."
     exit(0)
   else:
     zFile = zipfile.ZipFile(sys.argv[1])
